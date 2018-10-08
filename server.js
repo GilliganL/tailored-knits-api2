6d6d50db806e8f4e2e2903504b91664076d83278
Lynsey Powell <lynseympowell@gmail.com>
2018-10-08 10:04:23 -0600

    Add travis continuous integration

--- server.js
+++ server.js
@@ -1,7 +1,43 @@
 const express = require('express');
-const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+const { CLIENT_ORIGIN } = require('./config');
+const bodyParser = require('body-parser');
 
-app.use(express.static('public'));
+const app = express();
+const jsonParser = bodyParser.json();
+
+app.use(morgan('common'));
+
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+);
+
+app.get('/', (req, res) => {
+    return res.status(200)
+        .json({
+            users: [
+                {
+                    firstName: 'Lynsey',
+                    lastName: 'Powell'
+                },
+                {
+                    firstName: 'Adam',
+                    lastName: 'Phillips'
+                }
+            ]
+        });
+});
+
+app.post('/api/users', jsonParser, (req, res) => {
+    console.log(req.body)
+    res.json({
+        message: 'successful post'
+    })
+})
+
 app.listen(process.env.PORT || 8080);
 
 module.exports = { app };
